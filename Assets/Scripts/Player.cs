583a0fea69270098143bc448b2da24e5cf4b8ac2
Kimihiro n <kimihiro_n@pistatium.dip.jp>
2015-08-06 21:36:07 +0900

    Tutorial 09

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,8 +32,31 @@ public class Player : MonoBehaviour
 		// 移動する向きを求める
 		Vector2 direction = new Vector2 (x, y).normalized;
 		
-		// 移動
-		spaceship.Move (direction);
+		// 移動の制限
+		Move (direction);
+	}
+
+	// 機体の移動
+	void Move (Vector2 direction)
+	{
+		// 画面左下のワールド座標をビューポートから取得
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+		
+		// 画面右上のワールド座標をビューポートから取得
+		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+		
+		// プレイヤーの座標を取得
+		Vector2 pos = transform.position;
+		
+		// 移動量を加える
+		pos += direction  * spaceship.speed * Time.deltaTime;
+		
+		// プレイヤーの位置が画面内に収まるように制限をかける
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+		
+		// 制限をかけた値をプレイヤーの位置とする
+		transform.position = pos;
 	}
 
 	// ぶつかった瞬間に呼び出される
